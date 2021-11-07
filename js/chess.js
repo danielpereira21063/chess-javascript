444ec5756f3b3bc2045d8c3bb4506849618a58b2
Daniel Sanches <danielsanches6301@gmail.com>
2021-11-07 13:51:03 -0300

    Adicionando peças ao tabuleiro

--- js/chess.js
+++ js/chess.js
@@ -1,29 +1,84 @@
 $(function () {
+    var initialPosition = {
+        //black
+        a8: 'rook-black',
+        b8: 'knight-black',
+        c8: 'bishop-black',
+        d8: 'queen-black',
+        e8: 'king-black',
+        f8: 'bishop-black',
+        g8: 'knight-black',
+        h8: 'rook-black',
+
+        a7: 'pawn-black',
+        b7: 'pawn-black',
+        c7: 'pawn-black',
+        d7: 'pawn-black',
+        e7: 'pawn-black',
+        f7: 'pawn-black',
+        g7: 'pawn-black',
+        h7: 'pawn-black',
+
+
+        //white
+        a2: 'pawn-white',
+        b2: 'pawn-white',
+        c2: 'pawn-white',
+        d2: 'pawn-white',
+        e2: 'pawn-white',
+        f2: 'pawn-white',
+        g2: 'pawn-white',
+        h2: 'pawn-white',
+        
+        a1: 'rook-white',
+        b1: 'knight-white',
+        c1: 'bishop-white',
+        d1: 'queen-white',
+        e1: 'king-white',
+        f1: 'bishop-white',
+        g1: 'knight-white',
+        h1: 'rook-white'
+    };
+
+    function newGame() {
+        $('.square-board').each(function(){
+            var square = $(this);
+            
+            var sq = square.attr('id');
+            if(objSearchIndex(initialPosition, sq) != null) {
+                square.html(`
+                <div class="piece ${initialPosition[sq]}"></div>`);
+            }
+        });
+
+    }
 
     function printBoard() {
         var light = 1;
         var columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-        for (var l = 8; l >= 1; l--) {
+        for (var l = 8; l >= 1; --l) {
             for (var c = 0; c < columns.length; ++c) {
-                var square = columns[c]+1;
+                var sq = columns[c]+l;
                 var ligthdark = (light == 1 ? 'light': 'dark');
-                $('.board').append(`<div class="square-board ${ligthdark}" id="${square}">${square}</div>`);
-                light^=1;
+                $('.board').append(`<div class="square-board ${ligthdark}" id="${sq}"></div>`);
+                light ^= 1;
             }
-            light^=1;
+            light ^= 1;
         }
     }
 
     printBoard();
+    newGame();
 
     function objSearch(obj, valor) {
         var retorno = null;
 
         $.each(obj, function (i, val) {
             if (val == valor) {
-                return i;
+                retorno = i;
             }
         });
+
         return retorno;
     }
 
@@ -33,11 +88,10 @@ $(function () {
 
         $.each(obj, function (i, val) {
             if (i == index) {
-                return i;
+                retorno = i;
             }
         });
 
         return retorno;
     }
-
 });
